e31a0c14a1d1cdca6761baf48959f6e13cb909be
DwiPratiwi2002 <128199835+DwiPratiwi2002@users.noreply.github.com>
2023-04-23 00:16:10 +0800

    Add files via upload

---
+++ pwu/sesi5/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Selamat Datang Di Halaman Login, Silahkan Login Terlebih Dahulu</h2>
+    
+<form action="login.php" method="POST">
+        <div>
+            Username <br>
+            <input type="text" name="txUSER">
+        </div>
+        <div>
+            Password <br>
+            <input type="password" name="txPASSKEY">
+        </div>
+
+        <p><div>
+            <button type="submit">Login</button>
+        </p></div>
+
+        <p><div>
+        <button type ="submit"><a href="registrasi.php">Registrasi</button>
+        </p></div>
+</form>
+
+</body>
+</html>
